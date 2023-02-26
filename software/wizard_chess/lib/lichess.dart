880ab1ad8ea45c2e10c417c630ce59231ea14d53
BurningKarl <10158964+BurningKarl@users.noreply.github.com>
2023-02-26 22:08:26 +0000

    fix LichessClient json handling

--- software/wizard_chess/lib/lichess.dart
+++ software/wizard_chess/lib/lichess.dart
@@ -15,7 +15,7 @@ class LichessClient {
   Future<dynamic> challengeAi() async {
     var response =
         await _dio.post('/challenge/ai', data: {'level': 1, 'color': 'white'});
-    return response.data.json;
+    return response.data;
   }
 
   Future<Stream<dynamic>> streamBoardGameState({required String gameId}) async {
@@ -29,7 +29,8 @@ class LichessClient {
       {required String gameId, required Move move}) async {
     String uciMove =
         move.fromAlgebraic + move.toAlgebraic + (move.promotion?.name ?? "");
+    print("uciMove: $uciMove");
     var response = await _dio.post('/board/game/$gameId/move/$uciMove');
-    assert (response.data.json['ok']);
+    assert (response.data['ok']);
   }
 }
